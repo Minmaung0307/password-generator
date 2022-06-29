c706899d3f492dff960256f517105f96562c85ba
Min Maung <minmaung0307@gmail.com>
2022-06-29 14:14:58 -0400

    first commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,21 @@
 // Assignment code here
-
+function generatePassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var passwordLength = 12;
+        var password = "";
+        for (var i = 0; i <= passwordLength; i++) {
+            var randomNumber = Math.floor(Math.random() * chars.length);
+            password += chars.substring(randomNumber, randomNumber +1);
+        }
+        return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
-function writePassword() {
+function writePassword(lower, upper, number, symbol, length) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
